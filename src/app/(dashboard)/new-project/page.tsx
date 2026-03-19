45b2fe87f2be3105c31be3c8ab87716bba0ee0d5
unknown <arielgomezmendez94@gmail.com>
2026-03-19 13:33:08 -0400

    Implement the route for the form of creating a new project

---
+++ src/app/(dashboard)/new-project/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <section className="max-w-3xl">
+      <h1 className="mb-6 text-3xl font-bold text-slate-900">New Project</h1>
+      
+    </section>
+  );
+}

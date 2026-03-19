06d28b5ff2cd2c76147244fb1a2dc436e1551fb3
unknown <arielgomezmendez94@gmail.com>
2026-03-19 14:30:17 -0400

    Create the new project with the form data

--- src/app/components/NewProjectForm.tsx
+++ src/app/components/NewProjectForm.tsx
@@ -1,9 +1,35 @@
+"use client";
+
+import { useState } from "react";
+import { Project, ProjectStatus } from "../types/projectType";
+
 export default function NewProjectForm() {
+  /* States to save the info of form inputs */
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [status, setStatus] = useState<ProjectStatus>("Active");
+  const [date, setDate] = useState("");
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const newProject: Project = {
+      id: crypto.randomUUID(),
+      title: title.trim(),
+      description: description.trim(),
+      status,
+      date,
+    };
+  };
+
   return (
     <section className="max-w-3xl">
       <h1 className="mb-6 text-3xl font-bold text-slate-900">New Project</h1>
       {/* Form */}
-      <form className="rounded-2xl border border-slate-200 bg-white p-8 shadow-sm">
+      <form
+        onSubmit={handleSubmit}
+        className="rounded-2xl border border-slate-200 bg-white p-8 shadow-sm"
+      >
         {/*Input title */}
         <div className="mb-5">
           <label
@@ -15,6 +41,8 @@ export default function NewProjectForm() {
           <input
             id="title"
             type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
             className="w-full rounded-lg border border-slate-300 px-3 py-2 text-sm text-slate-900 outline-none focus:border-slate-400"
             placeholder="Enter project title"
           />
@@ -30,6 +58,8 @@ export default function NewProjectForm() {
           <textarea
             id="description"
             rows={5}
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
             className="w-full rounded-lg border border-slate-300 px-3 py-2 text-sm text-slate-900 outline-none focus:border-slate-400"
             placeholder="Enter project description"
           />
@@ -44,6 +74,8 @@ export default function NewProjectForm() {
           </label>
           <select
             id="status"
+            value={status}
+            onChange={(e) => setStatus(e.target.value as ProjectStatus)}
             className="w-full rounded-lg border border-slate-300 px-3 py-2 text-sm text-slate-900 outline-none focus:border-slate-400"
           >
             <option value="Active">Active</option>
@@ -61,6 +93,8 @@ export default function NewProjectForm() {
           <input
             id="date"
             type="date"
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
             className="w-full rounded-lg border border-slate-300 px-3 py-2 text-sm text-slate-900 outline-none focus:border-slate-400"
           />
         </div>

d94dceec5f9cf0e1971a6cfedcbd97062af2d3af
unknown <arielgomezmendez94@gmail.com>
2026-03-19 15:30:25 -0400

    Display all projects from localStorage and mock

--- src/app/components/ProjectView.tsx
+++ src/app/components/ProjectView.tsx
@@ -3,7 +3,7 @@
 import ProjectFilter from "./ProjectFilter";
 import ProjectList from "./ProjectList";
 import { Project } from "../types/projectType";
-import { useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 
 interface ProjectsViewProps {
   projects: Project[];
@@ -11,11 +11,23 @@ interface ProjectsViewProps {
 
 export default function ProjectView({ projects }: ProjectsViewProps) {
   const [statusFilter, setStatusFilter] = useState("");
+  const [allProjects, setAllProjects] = useState<Project[]>(projects);
+
+  useEffect(()=>{
+    const storedProjects = localStorage.getItem("projects");
+    //console.log("Stored project ", storedProjects)
+    if (storedProjects) {
+      const parsedProjects: Project[] = JSON.parse(storedProjects);
+      setAllProjects([...parsedProjects, ...projects]);
+    } else {
+      setAllProjects(projects);
+    }
+  },[projects]);
 
   const filteredProjects = useMemo(() => {
-    if (!statusFilter) return projects;
-    return projects.filter((project) => project.status === statusFilter);
-  }, [statusFilter,projects]);
+    if (!statusFilter) return allProjects;
+    return allProjects.filter((project) => project.status === statusFilter);
+  }, [statusFilter,allProjects]);
 
   return (
     <>
